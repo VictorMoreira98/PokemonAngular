6e139820ad8e711266e9c537dc56a9f917fbaf83
VictorMoreira98 <39058177+VictorMoreira98@users.noreply.github.com>
2021-01-21 18:54:40 -0300

    Home e Header
    Criação dos Modules Header e Home e seus componentes

---
+++ PokemonAngular/src/app/modules/home/home.module.ts
@@ -0,0 +1,19 @@
+import { GamesModule } from './../games/games.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { HeaderModule } from 'src/app/core/header/header.module';
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    HeaderModule,
+    GamesModule
+  ]
+})
+export class HomeModule { }
